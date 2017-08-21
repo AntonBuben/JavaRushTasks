95ccba4e9424a8e75e0b193a26d9d0d1d45b40a8
Anton Buben <2900551@mail.ru>
2017-08-21 16:23:23 +0300

    LastTask 1524 (Level l5)

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+    public SubSolution(int i, int j) {
+        super(i, j);
+    }
+
+    SubSolution(int i, int j, int k) {
+        super(i, j, k);
+    }
+
+    protected SubSolution(int i, int j, int k, int l) {
+        super(i, j, k, l);
+    }
+}
