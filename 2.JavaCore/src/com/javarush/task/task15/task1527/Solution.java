6b560ec9a0616a9715dcd972f961da084ab734eb
Anton Buben <2900551@mail.ru>
2017-08-23 21:50:40 +0300

    LastTask 1515 (Level 15 done)

--- 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -3,6 +3,9 @@ package com.javarush.task.task15.task1527;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /* 
 Парсер реквестов
@@ -28,6 +31,32 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String url = reader.readLine();
         String subUrl  = url.substring(url.indexOf("?")+1);
+
+        String [] tokens = subUrl.split("(\\&+)|(\\?+)");
+
+        ArrayList<String> objList = new ArrayList<String>();
+        Pattern objMutch = Pattern.compile("^obj=.*");
+        for(String a:tokens){
+            if(!a.equals("")){
+                Matcher matcher = objMutch.matcher(a);
+                if(matcher.find()){ objList.add(a.substring(a.indexOf("=")+1)); }
+                if(a.contains("=")){ System.out.print(a.substring(0,(a.indexOf("="))) +" "); }
+                else {System.out.print(a+" ");}
+            }
+        }
+
+        Pattern doublePattern = Pattern.compile("[0-9.]");
+        Pattern stringPattern = Pattern.compile("[A-Za-z]");
+
+        System.out.println();
+        for(String a:objList){
+            Matcher matcher = stringPattern.matcher(a);
+            Matcher matcher1 = doublePattern.matcher(a);
+            if(matcher.find()){alert(a);}
+            else  if(matcher1.find()){alert(Double.parseDouble(a));}
+        }
+
+
     }
 
     public static void alert(double value) {
